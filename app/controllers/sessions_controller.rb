b50e6877d1dc4cc1ff80a731c201f62b94c8628d
m3lD <matiasr.mdq@gmail.com>
2014-12-31 15:18:08 -0300

    9.3 Friendly forwarding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
             log_in user
             #boolean? ? do_one_thing : do_something_else
             params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-            redirect_to user
+            redirect_back_or user   #Redirect to the requested URL if it exist
         else
             #Error message
             flash.now[:danger] = 'Invalid email/password combination'
